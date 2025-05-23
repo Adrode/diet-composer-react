16b22f9c8cde56af054e02078226e811c9874611
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-05-23 19:08:09 +0200

    BMR container styling and form prepared

--- src/features/BMR/index.js
+++ src/features/BMR/index.js
@@ -1,8 +1,54 @@
+import {
+  BMRSummary,
+  Header,
+  Subheader,
+  MacroContainer,
+  Macro,
+  BMRCalculator,
+  LabelsContainer,
+  Label,
+  InputsContainer,
+  Input,
+  Submit,
+} from "./styled";
 
 export const BMR = () => {
   return (
     <>
-      kekw
+      <BMRSummary>
+        <Header>BMR Calculator</Header>
+        <Subheader>Your daily calorie and macronutrient requirements:</Subheader>
+        <MacroContainer>
+          <Macro>Kcal: {0}</Macro>
+          <Macro>Protein: {0}g</Macro>
+          <Macro>Fat: {0}g</Macro>
+          <Macro>Carbs: {0}g</Macro>
+        </MacroContainer>
+        <Subheader>Your per meal calorie and macronutrient requirements:</Subheader>
+        <MacroContainer>
+          <Macro>Kcal: {0}</Macro>
+          <Macro>Protein: {0}g</Macro>
+          <Macro>Fat: {0}g</Macro>
+          <Macro>Carbs: {0}g</Macro>
+        </MacroContainer>
+      </BMRSummary>
+      <BMRCalculator>
+        <LabelsContainer>
+          <Label>Height [cm]:</Label>
+          <Label>Weight [kg]:</Label>
+          <Label>Age:</Label>
+          <Label>Meals per day:</Label>
+          <Label>Activity:</Label>
+        </LabelsContainer>
+        <InputsContainer>
+          <Input type="number"></Input>
+          <Input type="number"></Input>
+          <Input type="number"></Input>
+          <Input type="number"></Input>
+          <Input type="number"></Input>
+        </InputsContainer>
+        <Submit type="button">Count!</Submit>
+      </BMRCalculator>
     </>
   )
 };
--- src/features/BMR/styled.js
+++ src/features/BMR/styled.js
@@ -1,7 +1,67 @@
 import styled from "styled-components";
 
-export const StyledBMR = styled.section`
-    padding: 20px 40px;
+export const BMRSummary = styled.article`
+    margin: 0 0 40px 0;
+`;
+
+export const Header = styled.h2`
+    margin: 0 0 20px 0;
+    font-size: 28px;
+    font-weight: normal;
+`;
+
+export const Subheader = styled.h3`
+    font-size: 22px;
+    font-weight: normal;
+    opacity: 60%;
+`;
+
+export const MacroContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+
+    padding-bottom: 10px;
+    border-bottom: 2px solid ${({ theme }) => theme.color.grey};
+`;
+
+export const Macro = styled.p`
+    margin: 0;
+`;
+
+export const BMRCalculator = styled.form`
+    border: 2px solid ${({ theme }) => theme.color.grey};
+    box-shadow: 5px 5px 15px -2px;
+    padding: 20px;
     width: 100%;
-    height: max-content;
+
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 20px 40px;
+`;
+
+export const LabelsContainer = styled.div`
+    display: grid;
+    grid-template-rows: repeat(5, 1fr);
+    gap: 20px;
+`;
+
+export const Label = styled.label``;
+
+export const InputsContainer = styled.div`
+    display: grid;
+    grid-template-rows: repeat(5, 1fr);
+    gap: 20px;
+`;
+
+export const Input = styled.input`
+    padding: 0;
+`;
+
+export const Submit = styled.button`
+    grid-column-start: 1;
+    grid-column-end: -1;
+    width: fit-content;
+    justify-self: center;
+    padding: 10px 30px;
 `;
